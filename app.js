bf7c7c6d8a620fa0fe91845d6d4a614d3d3106ed
Dmytro Hridin <v-dmytro.hridin@lionbridge.com>
2021-04-03 11:03:52 +0300

    implement example to work with cosmosdb

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const app = express();
+const CosmosClient = require("@azure/cosmos").CosmosClient;
+
 const port = process.env.PORT || 80
-app.get("/", (request, response) => {
-    response.send("<h2>Hello UCU!</h2>");
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+const client = new CosmosClient({
+    endpoint: '<enpoint>', 
+    key: '<key>',
+    });
+
+
+app.get("/dbId", (request, response) => {
+    const dbId = client.database('test_ucu_db').id;
+    response.send(dbId);
+});
+app.get("/file/:id", async (request, response) => {
+    const id = request.params.id;
+    const db = client.database('test_ucu_db');
+    const container = db.container('file_info');
+      
+    const { resource: item } = await container.item(id, id).read();
+    response.send(item);
 });
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
